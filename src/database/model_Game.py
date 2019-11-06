f9ff18e41faabb7e36afca8464798acd3b795f99
Mike Gross <michael3gross@gmail.com>
2019-11-06 18:15:57 -0500

    initial file creation

---
+++ src/database/model_Game.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from config import db_config, engine
+
+Base = declarative_base()
+
+class Game(Base):
+    __tablename__ = 'game'
+    id = Column('GameId', Integer, primary_key=True)
+    TeamHome = Column(Integer)
+    TeamAway = Column(Integer)
+    Season = Column(String(20))
+    Winner = Column(Integer)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
---
+++ src/database/model_Team.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from config import db_config
+
+Base = declarative_base()
+
+class Team(Base):
+    __tablename__ = 'teams'
+    TeamName = Column(String)
+    TeamID = Column(Integer, primary_key=True)
+
+engine = create_engine(URL(**db_config), echo=True)
